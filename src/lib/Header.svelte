bde45585ddc04e73794f2456f42352f2df0c0519
do-ald533 <douglas.aldred533@gmail.com>
2022-07-20 00:54:50 -0300

    adding svg files and components

---
+++ src/lib/Header.svelte
@@ -0,0 +1,5 @@
+<header>
+    <img src="profile.png" alt="" srcset="">
+    <h1>@do-ald533</h1>
+</header>
+
---
+++ src/lib/Link.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let svg: string;
+	export let link: string;
+	export let name: string;
+</script>
+
+<li>
+	<a href={link}>
+		<img src={svg} alt="" />
+		{name}
+	</a>
+</li>
+
+<style lang="scss">
+</style>
